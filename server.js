c395857f7245213b2c9c0b9cf78c5d0a822ffe26
cxrtut <160480710+cxrtut@users.noreply.github.com>
2025-02-18 15:31:37 +0200

    api/server and index.js

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(cors());
 
 app.use("/api/news", require("./routes/newsRoutes"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export Express API as a serverless function
+module.exports = app;
