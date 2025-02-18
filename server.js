6c9f3e834bece032238fa7b44bf43767a1c8840b
cxrtut <160480710+cxrtut@users.noreply.github.com>
2025-02-18 15:47:38 +0200

    vercel update

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(cors());
 
 app.use("/api/news", require("./routes/newsRoutes"));
 
-// Export Express API as a serverless function
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
